5a2ff9635960893f99dbb50c8703c776915e3f7f
Adarsh Dixit <adarshdixit1995@gmail.com>
2020-01-25 19:41:48 +0530

    - static file middlware - default file middleware - file server middleware

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -28,7 +28,7 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,30 +46,31 @@ namespace EmployeeManagement
             //    });
             //});
 
-            // Middleware 1
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MiddleWare 1: Request");
-                await next();
-                logger.LogInformation("MiddleWare 1: Response");
-            });
+            //Inject ILogger<Startup> logger in Configure()
+            //// Middleware 1
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MiddleWare 1: Request");
+            //    await next();
+            //    logger.LogInformation("MiddleWare 1: Response");
+            //});
 
 
-            // Middleware 2
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MiddleWare 2: Request");
-                await next();
-                logger.LogInformation("MiddleWare 2: Response");
-            });
+            //// Middleware 2
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MiddleWare 2: Request");
+            //    await next();
+            //    logger.LogInformation("MiddleWare 2: Response");
+            //});
 
-            //Middleware 3
-            app.Run(async (context) =>
-            {
-                logger.LogInformation("MiddleWare 3: Request");
-                await context.Response.WriteAsync("MiddleWare 3: Final Response");
-                logger.LogInformation("MiddleWare 3: Response");
-            });
+            ////Middleware 3
+            //app.Run(async (context) =>
+            //{
+            //    logger.LogInformation("MiddleWare 3: Request");
+            //    await context.Response.WriteAsync("MiddleWare 3: Final Response");
+            //    logger.LogInformation("MiddleWare 3: Response");
+            //});
 
             //EmployeeManagement.Startup: Information: MiddleWare 1: Request
             //EmployeeManagement.Startup: Information: MiddleWare 2: Request
@@ -77,6 +78,26 @@ namespace EmployeeManagement
             //EmployeeManagement.Startup: Information: MiddleWare 3: Response
             //EmployeeManagement.Startup: Information: MiddleWare 2: Response
             //EmployeeManagement.Startup: Information: MiddleWare 1: Response
+
+            // Adding default middleware (will land on Default.html)
+            //app.UseDefaultFiles();
+
+            // Modifying the default middleware
+            //var defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("Foo.html");
+            //app.UseDefaultFiles(defaultFilesOptions);
+
+            // Adding static files from wwwroot (https://localhost:44380/images/Image.jpg)
+            app.UseStaticFiles();
+
+            // Adding file server middleware to replace UseDefaultFiles and UseStaticFiles
+            app.UseFileServer();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Final Response");
+            });
         }
     }
 }
